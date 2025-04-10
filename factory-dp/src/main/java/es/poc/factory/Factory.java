20199fb3dbefa50c84d1ab43e7aa7c4a57ebf3a1
bogdan <bberghie@gmail.com>
2025-04-10 21:50:46 +0200

    refactoring

---
+++ factory-dp/src/main/java/es/poc/factory/Factory.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.poc.factory;
+
+import es.poc.factory.entity.payment.Payment;
+import es.poc.factory.entity.payment.PaymentFactory;
+import es.poc.factory.entity.payment.enums.PaymentType;
+
+/**
+ *
+ * @author bgd
+ */
+public class Factory {
+
+    public static void main(String[] args) {
+        //Payment factory inialization
+        PaymentFactory paymentFactory = new PaymentFactory();
+        
+        // Creation of the different types of Payment by the PaymentType Enum
+        Payment paymentVisa = paymentFactory.getPayment(PaymentType.VISA);
+        System.out.println(paymentVisa.howAmI());
+        
+        Payment paymentPayPal = paymentFactory.getPayment(PaymentType.PAYPAL);
+        System.out.println(paymentPayPal.howAmI());
+        
+        System.out.println(paymentFactory.getPayment(PaymentType.GOOGLE_PAY).howAmI());
+        
+        System.out.println(paymentFactory.getPayment(PaymentType.APPLE_PAY).howAmI());
+        
+    }
+}
