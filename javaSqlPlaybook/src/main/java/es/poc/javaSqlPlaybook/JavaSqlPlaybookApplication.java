9f25c7729d5577a75d30d813c2c35252d1d5ac39
bogdan <bberghie@gmail.com>
2025-04-24 19:25:56 +0200

    sql playing

---
+++ javaSqlPlaybook/src/main/java/es/poc/javaSqlPlaybook/JavaSqlPlaybookApplication.java
@@ -0,0 +1,13 @@
+package es.poc.javaSqlPlaybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSqlPlaybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSqlPlaybookApplication.class, args);
+	}
+
+}
